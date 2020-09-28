1d4d71264bb0572b7c197f1138d867a18c9de8d1
kabirkedia <kabirkedia0111@gmail.com>
2020-09-29 01:31:59 +0530

    Updates

---
+++ precache-manifest.ef11fc2e3645c5eccdf8959388dc290a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2032b8001ed7d01d62fb7904e51681",
+    "url": "/piano/index.html"
+  },
+  {
+    "revision": "ce3f4315a428d40b25c5",
+    "url": "/piano/static/css/main.e8359dee.chunk.css"
+  },
+  {
+    "revision": "7ce74681d498dba699b1",
+    "url": "/piano/static/js/2.75c925ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/piano/static/js/2.75c925ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3f4315a428d40b25c5",
+    "url": "/piano/static/js/main.b6679ee5.chunk.js"
+  },
+  {
+    "revision": "3bf3f42ad144e33d4b91",
+    "url": "/piano/static/js/runtime-main.887358fa.js"
+  }
+]);
